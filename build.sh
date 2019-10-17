178a4840afcf6f3150081c997cde0576467349d4
Christopher A. Williamson <home@chrisaw.com>
2019-10-17 11:35:52 +0100

    Fix new image filenameg

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ docker run \
   -v $(pwd)/out:/out \
   buildkernel \
   /bin/bash -c " \
-  git clone https://github.com/cawilliamson/samsung_sm907b_kernel /usr/src/kernel && \
+  git clone -b custom https://github.com/cawilliamson/samsung_sm907b_kernel /usr/src/kernel && \
   cd /usr/src/kernel && \
   bash build_kernel.sh && \
   cd /var/tmp && \
   cp -fv /common/imgs/recovery.img recovery.img && \
   /common/bin/magiskboot unpack recovery.img && \
-  cp -fv /usr/src/kernel/arch/arm64/boot/Image kernel && \
+  cp -fv /usr/src/kernel/arch/arm64/boot/Image-dtb kernel && \
   /common/bin/magiskboot repack recovery.img new-recovery.img && \
   cp -fv /var/tmp/new-recovery.img /out/patched-recovery.img && \
   chmod -v 777 /out/*"
