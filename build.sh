868d7d3a2d87e6dc6311de52f779f5ab468a3c37
Christopher A. Williamson <home@chrisaw.com>
2019-10-18 14:57:42 +0100

    Fix branch name

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ docker run \
   -v $(pwd)/out:/out \
   buildkernel \
   /bin/bash -c " \
-  git clone -b custom https://github.com/cawilliamson/samsung_sm907b_kernel /usr/src/kernel && \
+  git clone -b leankernel-9.0 https://github.com/cawilliamson/samsung_sm907b_kernel /usr/src/kernel && \
   cd /usr/src/kernel && \
   bash build_kernel.sh && \
   cp -fv /usr/src/kernel/arch/arm64/boot/Image-dtb /out/Image-dtb && \
