337f4a4eab5000d63e6e88d8f51d23b3e1789812
Christopher A. Williamson <home@chrisaw.com>
2019-10-18 11:09:34 +0100

    Stop building full recovery image - not needed

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 docker build -t buildkernel .
 docker run \
-  -v $(pwd)/common:/common \
   -v $(pwd)/out:/out \
   buildkernel \
   /bin/bash -c " \
@@ -10,10 +9,4 @@ docker run \
   cd /usr/src/kernel && \
   bash build_kernel.sh && \
   cp -fv /usr/src/kernel/arch/arm64/boot/Image-dtb /out/Image-dtb && \
-  cd /var/tmp && \
-  cp -fv /common/imgs/recovery.img recovery.img && \
-  /common/bin/magiskboot unpack recovery.img && \
-  cp -fv /usr/src/kernel/arch/arm64/boot/Image-dtb kernel && \
-  /common/bin/magiskboot repack recovery.img new-recovery.img && \
-  cp -fv /var/tmp/new-recovery.img /out/patched-recovery.img && \
   chmod -v 777 /out/*"
