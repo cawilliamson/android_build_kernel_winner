2c93d6c9310e229008249ba86da75500a6e78db5
Christopher A. Williamson <home@chrisaw.com>
2019-11-09 13:35:22 +0000

    Adapt script to build latest winnerx kernel

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ docker run \
   buildkernel \
   /bin/bash -c " \
   git clone \
-    -b leankernel-9.0 \
-    https://github.com/cawilliamson/android_kernel_samsung_winner \
+    -b master \
+    https://github.com/cawilliamson/android_kernel_samsung_winnerx \
     /usr/src/kernel && \
   cd /usr/src/kernel && \
-  bash build_kernel_${1}.sh && \
+  bash build_kernel.sh && \
   cp -fv /usr/src/kernel/arch/arm64/boot/Image-dtb /out/Image-dtb && \
   chmod -v 777 /out/*"
